7b3fca9e85d3dad689f72e7b04206ac453611883
Anuj Hooda <hoodaanuj03@gmail.com>
2026-06-10 11:30:29 +0530

    Feat : adding scroll logic

--- client/src/Components/ScrollTotop.jsx
+++ client/src/Components/ScrollTotop.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import { useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
 
-const ScrollTotop = () => {
-  return (
-    <div>ScrollTotop</div>
-  )
-}
+const ScrollToTop = () => {
+  const { pathname } = useLocation();
 
-export default ScrollTotop
+  useEffect(() => {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  }, [pathname]);
+
+  return null;
+};
+
+export default ScrollToTop;
